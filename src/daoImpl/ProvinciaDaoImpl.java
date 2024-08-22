766c808836142090aad7785c8d732293fc7bfdc8
Guada Juricich <guadajuricich@gmail.com>
2024-08-22 14:50:17 -0300

    query y función get provincias

--- src/daoImpl/ProvinciaDaoImpl.java
+++ src/daoImpl/ProvinciaDaoImpl.java
@@ -15,6 +15,42 @@ import daoImpl.PaisDaoImpl;
 public class ProvinciaDaoImpl implements ProvinciaDao{
 
 	private static final String list = "SELECT * FROM provincias";
+	private static final String get = "SELECT * FROM provincias WHERE provincia_id = ?";
+	
+
+	@Override
+	public Provincia get(int prov_id) {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		
+		try {
+			statement = conexion.prepareStatement(get);
+			statement.setInt(1, prov_id);
+			ResultSet result_set = statement.executeQuery();
+			while(result_set.next()) {
+				
+				String nombre_provincia = result_set.getString("nombre");
+				int pais_id = result_set.getInt("pais_id");
+				int provincia_id = result_set.getInt("provincia_id");
+				PaisDaoImpl paisDaoImpl = new PaisDaoImpl();
+				Pais pais = paisDaoImpl.get(pais_id);
+		
+				Provincia provincia = new Provincia(provincia_id,nombre_provincia,pais);
+				return provincia;
+			}
+		}		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 
 	@Override
 	public ArrayList<Provincia> list() {
