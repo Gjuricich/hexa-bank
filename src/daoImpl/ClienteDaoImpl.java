2878baeb593a53d9cb06aae9b575f8f9ce1336f1
Guada Juricich <guadajuricich@gmail.com>
2024-08-22 14:34:53 -0300

    querys y funciones existeCliente y getPorIdUsuario

--- src/daoImpl/ClienteDaoImpl.java
+++ src/daoImpl/ClienteDaoImpl.java
@@ -19,12 +19,15 @@ import daoImpl.LocalidadDaoImpl;
 
 public class ClienteDaoImpl implements ClienteDao {
     
-        private static final String update = "UPDATE clientes SET cuil = ? , nombre = ?, apellido = ?, sexo = ?, nacionalidad = ?, fecha_nacimiento = ?, direccion = ?, localidad_id = ?, provincia_id = ?, correo_electronico = ?, telefono = ? WHERE dni = ?";
-        private static final String get = "SELECT * FROM clientes WHERE dni = ?";
+	private static final String update = "UPDATE clientes SET cuil = ? , nombre = ?, apellido = ?, sexo = ?, nacionalidad = ?, fecha_nacimiento = ?, direccion = ?, localidad_id = ?, provincia_id = ?, correo_electronico = ?, telefono = ? WHERE dni = ?";
 	private static final String insert = "INSERT INTO clientes(dni, cuil, nombre ,apellido ,sexo ,nacionalidad ,fecha_nacimiento ,direccion ,localidad_id ,correo_electronico ,telefono ,usuario_id ,provincia_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?)";	
-        private static final String delete = "CALL SP_BAJA_CLIENTE(?,?);";
-        private static final String prestamosPorCliente= "SELECT COUNT(*) FROM prestamos as p INNER JOIN cuentas c ON p.numero_cuenta = c.numero_cuenta INNER JOIN clientes cl ON c.dni = cl.dni WHERE cl.dni = ? AND p.estado_prestamo <> 'Rechazado' AND p.estado = 'Vigente'";
-
+	private static final String delete = "CALL SP_BAJA_CLIENTE(?,?);";
+	private static final String list = "SELECT * FROM clientes WHERE estado = 1";
+	private static final String get = "SELECT * FROM clientes WHERE dni = ?";
+	private static final String getId = "SELECT * FROM clientes WHERE usuario_id = ?";
+	private static final String queryExiste = "SELECT COUNT(*) FROM clientes WHERE dni = ?";
+	private static final String prestamosPorCliente= "SELECT COUNT(*) FROM prestamos as p INNER JOIN cuentas c ON p.numero_cuenta = c.numero_cuenta INNER JOIN clientes cl ON c.dni = cl.dni WHERE cl.dni = ? AND p.estado_prestamo <> 'Rechazado' AND p.estado = 'Vigente'";
+	
 	@Override
 	public boolean insert(Cliente cliente) {
 		try 
@@ -73,7 +76,7 @@ public class ClienteDaoImpl implements ClienteDao {
 	    return isInsertExitoso;
 	}
 
-        @Override
+	@Override
 	public String delete(Cliente cliente) {
 		try 
     	{
@@ -109,8 +112,8 @@ public class ClienteDaoImpl implements ClienteDao {
         }
 	    return respuesta;
 	}
-
-        public boolean prestamosPorCliente(String dni) {
+	
+	public boolean prestamosPorCliente(String dni) {
 	    try {
 	        Class.forName("com.mysql.jdbc.Driver");
 	    } catch (ClassNotFoundException e) {
@@ -138,7 +141,8 @@ public class ClienteDaoImpl implements ClienteDao {
 	    return tienePrestamos;
 	}
 
-        @Override
+
+	@Override
 	public boolean update(Cliente cliente) {
 	    try {
 	        Class.forName("com.mysql.jdbc.Driver");
@@ -178,8 +182,9 @@ public class ClienteDaoImpl implements ClienteDao {
 	    }
 	    return isUpdateExitoso;
 	}
-        
-        @Override
+
+
+	@Override
 	public Cliente get(String dni) {
 		try 
     	{
@@ -222,9 +227,119 @@ public class ClienteDaoImpl implements ClienteDao {
 		return null;
 	}
 	
+	@Override
+	public Cliente getPorIdUsuario(int idUsuario ) {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		
+		try {
+			statement = conexion.prepareStatement(getId);
+			statement.setInt(1, idUsuario );
+			ResultSet result_set = statement.executeQuery();
+			while(result_set.next()) {
+                UsuarioDaoImpl usuarioDaoImpl = new UsuarioDaoImpl();
+                LocalidadDaoImpl localidadDaoImpl = new LocalidadDaoImpl();
+				
 
+				Cliente cliente = new  Cliente(
+				    result_set.getString("dni"),
+					result_set.getString("cuil"),
+					result_set.getString("nombre"), 
+					result_set.getString("apellido"), 
+					result_set.getString("sexo").charAt(0),
+					result_set.getString("nacionalidad"), 
+					result_set.getDate("fecha_nacimiento"), 
+					result_set.getString("direccion"),
+					localidadDaoImpl.get(result_set.getInt("localidad_id")), 
+					result_set.getString("correo_electronico"),
+					result_set.getString("telefono"), 
+					usuarioDaoImpl.get(result_set.getInt("usuario_id"))
+				);
+				return cliente;
+			}
+		}		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+	@Override
+	public ArrayList<Cliente> list() {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+		ArrayList<Cliente> list_clientes = new ArrayList<Cliente>();
+		try {
+			Connection conexion = Conexion.getConexion().getSQLConexion();
+			Statement statement = conexion.createStatement();
+			ResultSet result_set = statement.executeQuery(list);
+			while(result_set.next()) {
+                UsuarioDaoImpl usuarioDaoImpl = new UsuarioDaoImpl();
+                LocalidadDaoImpl localidadDaoImpl = new LocalidadDaoImpl();
+				list_clientes.add(
+					new  Cliente(
+						result_set.getString("dni"),
+						result_set.getString("cuil"),
+						result_set.getString("nombre"), 
+						result_set.getString("apellido"), 
+						result_set.getString("sexo").charAt(0),
+						result_set.getString("nacionalidad"), 
+						result_set.getDate("fecha_nacimiento"), 
+						result_set.getString("direccion"),
+						localidadDaoImpl.get(result_set.getInt("localidad_id")), 
+						result_set.getString("correo_electronico"),
+						result_set.getString("telefono"), 
+						usuarioDaoImpl.get(result_set.getInt("usuario_id"))
+					)
+				);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list_clientes;
+	}
 
+	public boolean existeCliente(String dni) {
+		try 	
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+        
+        boolean existe = false;
+        
+        try {
+        	Connection conexion = Conexion.getConexion().getSQLConexion();
+        	PreparedStatement statement = conexion.prepareStatement(queryExiste);
+            statement.setString(1, dni);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                if (count > 0) {
+                    existe = true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+        return existe;
+	}
 
 	
 }
