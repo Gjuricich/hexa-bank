f09a2e4485a612c665f8f7b0a5aaa4f920ec4c1f
Guada Juricich <guadajuricich@gmail.com>
2024-08-22 14:47:16 -0300

    query y función get país

--- src/daoImpl/PaisDaoImpl.java
+++ src/daoImpl/PaisDaoImpl.java
@@ -12,7 +12,35 @@ import dao.PaisDao;
 public class PaisDaoImpl  implements PaisDao {
 
 	private static final String list = "SELECT * FROM paises";
-	
+	private static final String get = "SELECT * FROM paises WHERE pais_id = ?";
+
+	@Override
+	public Pais get(int pais_id) {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		
+		try {
+			statement = conexion.prepareStatement(get);
+			statement.setInt(1, pais_id);
+			ResultSet result_set = statement.executeQuery();
+			while(result_set.next()) {
+				String nombre_pais = result_set.getString("nombre");
+				int paisId = result_set.getInt("pais_id");
+				Pais pais = new Pais(paisId,nombre_pais);
+				return pais;
+			}
+		}		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	@Override
 	public ArrayList<Pais> list() {
 		try 
