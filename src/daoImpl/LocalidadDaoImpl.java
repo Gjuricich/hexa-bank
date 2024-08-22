a8e6910d448303364327f029eb658c9ba91712c5
Guada Juricich <guadajuricich@gmail.com>
2024-08-22 14:51:25 -0300

    query y función listar localidades

---
+++ src/daoImpl/LocalidadDaoImpl.java
@@ -0,0 +1,52 @@
+package daoImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import entidad.Localidad;
+import entidad.Provincia;
+import dao.LocalidadDao;
+import daoImpl.ProvinciaDaoImpl;
+
+public class LocalidadDaoImpl implements LocalidadDao {
+
+	private static final String list = "SELECT * FROM localidades";
+	
+	@Override
+	public ArrayList<Localidad> list() {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}
+		ArrayList<Localidad> list_localidades = new ArrayList<Localidad>();
+		try {
+			Connection conexion = Conexion.getConexion().getSQLConexion();
+			Statement statement = conexion.createStatement();
+			ResultSet result_set = statement.executeQuery(list);
+			while(result_set.next()) {
+				
+				int localidadId = result_set.getInt("localidad_id");
+				String nombre = result_set.getString("nombre");
+				int provincia_id = result_set.getInt("provincia_id");
+				
+				ProvinciaDaoImpl provinciaDaoImpl = new ProvinciaDaoImpl();
+				Provincia provincia = provinciaDaoImpl.get(provincia_id);
+				
+				Localidad localidad = new Localidad(localidadId,nombre,provincia);
+
+				list_localidades.add(localidad);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list_localidades;
+	}
+
+}
