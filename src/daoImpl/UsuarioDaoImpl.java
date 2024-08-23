7e2b3eacc40c3b7f9ecad7e80e21bf151b22c430
Alebardock <Alejandrod.morales@hotmail.com>
2024-08-23 20:03:03 -0300

    agrego funcion y query insert

--- src/daoImpl/UsuarioDaoImpl.java
+++ src/daoImpl/UsuarioDaoImpl.java
@@ -17,9 +17,42 @@ import daoImpl.Conexion;
 public class UsuarioDaoImpl implements UsuarioDao {
 
 	private static final String update = "UPDATE usuarios SET nombre_usuario = ?, password = ?,tipo_usuario_id = ? WHERE usuario_id = ?";
+	private static final String insert = "INSERT INTO usuarios(nombre_usuario, password, tipo_usuario_id) VALUES(?, ?, ?)";	
 	
-	
-	
+	@Override
+	public boolean insert(Usuario usuario) {
+		try 
+    	{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	} 
+		    PreparedStatement statement;
+		    Connection conexion = Conexion.getConexion().getSQLConexion();
+		    boolean isInsertExitoso = false;
+		    try
+		    {
+		        statement = conexion.prepareStatement(insert);
+		        statement.setString(1, usuario.getNombreUsuario());
+		        statement.setString(2,usuario.getPassword());
+		        statement.setInt(3,usuario.getTipoUsuario().getTipoUsuarioId());     
+		        if(statement.executeUpdate() > 0)
+		        {
+		            conexion.commit();
+		            isInsertExitoso = true;
+		        }
+		    } 
+		    catch (SQLException e) 
+		    {
+		        e.printStackTrace();
+		        try {
+		            conexion.rollback();
+		        } catch (SQLException e1) {
+		            e1.printStackTrace();
+		        }
+		    }
+		    return isInsertExitoso;
+	}
 
 	@Override
 	public boolean update(Usuario usuario) {
