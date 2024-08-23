8a9dc08afaeffcdaec388f309e53b448785bcc69
Alebardock <Alejandrod.morales@hotmail.com>
2024-08-22 22:44:33 -0300

    agrego funcion y query getById

--- src/daoImpl/TipoPrestamoDaoImpl.java
+++ src/daoImpl/TipoPrestamoDaoImpl.java
@@ -13,7 +13,9 @@ import entidad.TipoPrestamo;
 public class TipoPrestamoDaoImpl implements TipoPrestamoDao {
 	
 	private static final String get = "SELECT * FROM tipos_prestamo WHERE importe_total = ? and nro_cuotas = ?";
-   
+    private static final String getbyId = "SELECT * FROM tipos_prestamo WHERE tipo_prestamo_id = ?";
+    
+    
     @Override
     public TipoPrestamo get(int cuotas, BigDecimal monto) {
         try {
@@ -48,4 +50,35 @@ public class TipoPrestamoDaoImpl implements TipoPrestamoDao {
         return null;
     }
 
+    public TipoPrestamo get(int tipoprestamoid) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        PreparedStatement statement;
+        Connection conexion = Conexion.getConexion().getSQLConexion();
+        
+        try {
+            statement = conexion.prepareStatement(getbyId);
+            statement.setInt(1, tipoprestamoid);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                TipoPrestamo tipoPrestamo = new TipoPrestamo(
+                    resultSet.getInt("tipo_prestamo_id"),
+                    resultSet.getBigDecimal("importe_total"),
+                    resultSet.getBigDecimal("importe_intereses"),
+                    resultSet.getInt("nro_cuotas"),
+                    resultSet.getBigDecimal("cuota_mensual"),
+                    resultSet.getBigDecimal("interes_anual")
+                );
+                return tipoPrestamo;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
