eec5831b03aba0d972ba2fc4c5709b9059b37306
Alebardock <Alejandrod.morales@hotmail.com>
2024-08-29 17:44:51 -0300

    agrego servlet funcional

--- src/servlet/ServletAdminPrestamos.java
+++ src/servlet/ServletAdminPrestamos.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.ArrayList;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import entidad.Cliente;
 import entidad.Cuenta;
 import entidad.Prestamo;
@@ -45,8 +43,7 @@ public class ServletAdminPrestamos extends HttpServlet {
         TipoPrestamo auxTipoPrestamo = tipoPrestamoImpl.get(cuotasSeleccionadas, montoSeleccionado);
 
       
-       
-
+    
         request.setAttribute("Monto", auxTipoPrestamo.getImporteTotal());
         request.setAttribute("Cuotas", auxTipoPrestamo.getNroCuotas());
         request.setAttribute("interes", auxTipoPrestamo.getInteresAnual());
